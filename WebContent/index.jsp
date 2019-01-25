fa6f1dbb15c5123fca6bd618c47f656f8b69ec9d
vhellendoorn <vhellendoorn@live.nl>
2019-01-24 18:17:50 -0800

    Bulk update

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,6 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java"%>
-<%@ page import="map.Lookup" %>
-<%@ page import="map.Location" %>
+<%@ page import="edu.studyup.map.Lookup" %>
+<%@ page import="edu.studyup.entity.Location" %>
 <html>
 <head>
 	<meta name="viewport" content="initial-scale=1, maximum-scale=1">
@@ -77,9 +77,10 @@
 	if (place != null && !place.isEmpty()) {
        	Location loc = Lookup.lookupPlace(place);
        	if (loc != null) {
-        	double lat = loc.getLat();
-        	double lon = loc.getLon(); %>
-        	<script>gotoLoc(<%=lat%>, <%=lon%>);</script>
+        	double lat = loc.lat;
+        	double lon = loc.lon;
+        	double[] bounds = loc.bounds; %>
+        	<script>gotoLoc(<%=lat%>, <%=lon%>, [<%=bounds[0]%>, <%=bounds[1]%>, <%=bounds[2]%>, <%=bounds[3]%>]);</script>
        	<%}
        }%>
 </body>
--- src/edu/studyup/map/Lookup.java
+++ src/edu/studyup/map/Lookup.java
@@ -1,4 +1,4 @@
-package map;
+package edu.studyup.map;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,17 +11,30 @@ import java.net.URLEncoder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import edu.studyup.entity.Location;
+
 public class Lookup {
 	public static Location lookupPlace(String query) {
 		JSONArray results = queryURL(query);
 		if (results.isEmpty()) return null;
-		
-        JSONObject best = results.getJSONObject(0);
-        double lat = best.getDouble("lat");
-        double lon = best.getDouble("lon");
-        return new Location(lat, lon);
+		// For now, simply return the location for the top result
+		JSONObject best = results.getJSONObject(0);
+        return getLocation(best);
 	}
 
+	private static Location getLocation(JSONObject location) {
+        double lat = location.getDouble("lat");
+        double lon = location.getDouble("lon");
+        double[] bounds = new double[4];
+        JSONArray boundingBox = location.getJSONArray("boundingbox");
+        // Convert Nominatim API format (minLon, maxLon, minLat, maxLat) to OpenLayers format (minLat, minLon, maxLat, maxLon)
+        bounds[0] = boundingBox.getDouble(2);
+        bounds[1] = boundingBox.getDouble(0);
+        bounds[2] = boundingBox.getDouble(3);
+        bounds[3] = boundingBox.getDouble(1);
+        return new Location(lat, lon, bounds);
+	}
+	
 	private static JSONArray queryURL(String query) {
 		JSONArray results = new JSONArray();
 		try {
