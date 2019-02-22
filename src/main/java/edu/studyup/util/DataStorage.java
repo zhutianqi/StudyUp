ceb08f09b898869de3ac5fd0a2df51c85eecc86b
Yingjiao Zhai <yjzhai@ucdavis.edu>
2019-02-21 16:16:07 -0800

    SpotBugs

--- src/main/java/edu/studyup/util/DataStorage.java
+++ src/main/java/edu/studyup/util/DataStorage.java
@@ -14,5 +14,9 @@ import edu.studyup.entity.Event;
  * 
  */
 public class DataStorage {
-	public static Map<Integer, Event> eventData = new HashMap<Integer, Event>();
+	private static Map<Integer, Event> eventData = new HashMap<Integer, Event>();
+
+	public static Map<Integer, Event> getEventdata() {
+		return eventData;
+	}
 }
