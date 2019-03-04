2fda56e347c3ac9283dc6f3598cd1feecfda06bf
vhellendoorn <vhellendoorn@live.nl>
2019-03-04 08:31:29 -0800

    Wrap Jedis calls

--- src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
+++ src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
@@ -12,50 +12,60 @@ import redis.clients.jedis.Jedis;
 
 public class EventServiceImpl implements EventService {
 	
-	private Jedis redis;
 	private Gson gson;
 
 	public EventServiceImpl(String URL) {
-		this.redis = new Jedis("database", 8888);
 		this.gson = new Gson();
 	}
 	
 	@Override
 	public Event getEvent(String eventID) {
-		String eventString = this.redis.get(eventID);
-		return gson.fromJson(eventString, Event.class);
+		try (Jedis jedis = new Jedis("database", 8888)) {
+			String eventString = jedis.get(eventID);
+			return gson.fromJson(eventString, Event.class);
+		}
 	}
 	
 	@Override
 	public void createEvent(String eventID, Event event) {
-		String eventString = gson.toJson(event);
-		this.redis.set(eventID, eventString);
+		try (Jedis jedis = new Jedis("database", 8888)) {
+			String eventString = gson.toJson(event);
+			jedis.set(eventID, eventString);
+		}
 	}
 
 	@Override
 	public void updateEvent(String eventID, Event event) {
-		String eventString = gson.toJson(event);
-		this.redis.set(eventID, eventString);
+		try (Jedis jedis = new Jedis("database", 8888)) {
+			String eventString = gson.toJson(event);
+			jedis.set(eventID, eventString);
+		}
 	}
 
 	@Override
 	public long deleteEvent(String key) {
-		return this.redis.del(String.valueOf(key));
+		try (Jedis jedis = new Jedis("database", 8888)) {
+			return jedis.del(String.valueOf(key));
+		}
 	}
 
 	@Override
 	public List<Event> getAllEvents() {
-		List<Event> eventList = new ArrayList<>();
-		Set<String> keys = this.redis.keys("*");
-		for (String key: keys) {
-			Event event = getEvent(key);
-			eventList.add(event);
+		try (Jedis jedis = new Jedis("database", 8888)) {
+			List<Event> eventList = new ArrayList<>();
+			Set<String> keys = jedis.keys("*");
+			for (String key: keys) {
+				Event event = getEvent(key);
+				eventList.add(event);
+			}
+			return eventList;
 		}
-		return eventList;
 	}
 
 	@Override
 	public String deleteAll() {
-		return this.redis.flushAll();
+		try (Jedis jedis = new Jedis("database", 8888)) {
+			return jedis.flushAll();
+		}
 	}
 }
