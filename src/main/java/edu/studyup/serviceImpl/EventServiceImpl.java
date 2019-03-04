11ac930d6e9c997937833d75c3051c4874b8379f
unknown <vhellendoorn@live.nl>
2019-03-03 18:50:41 -0800

    Upgrade backend to Jedis

--- src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
+++ src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
@@ -8,57 +8,45 @@ import com.google.gson.Gson;
 
 import edu.studyup.entity.Event;
 import edu.studyup.service.EventService;
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.sync.RedisKeyCommands;
-import io.lettuce.core.api.sync.RedisStringCommands;
+import redis.clients.jedis.Jedis;
 
 public class EventServiceImpl implements EventService {
 	
-	private RedisClient redis;
+	private Jedis redis;
 	private Gson gson;
 
 	public EventServiceImpl(String URL) {
-		this.redis = RedisClient.create("redis://database:8888");
+		this.redis = new Jedis("database", 8888);
 		this.gson = new Gson();
 	}
 	
 	@Override
 	public Event getEvent(String eventID) {
-		StatefulRedisConnection<String, String> connection = this.redis.connect();
-		RedisStringCommands<String, String> sync = connection.sync();
-		String eventString = sync.get(eventID);
+		String eventString = this.redis.get(eventID);
 		return gson.fromJson(eventString, Event.class);
 	}
 	
 	@Override
 	public void createEvent(String eventID, Event event) {
-		StatefulRedisConnection<String, String> connection = this.redis.connect();
-		RedisStringCommands<String, String> sync = connection.sync();
 		String eventString = gson.toJson(event);
-		sync.set(eventID, eventString);
+		this.redis.set(eventID, eventString);
 	}
 
 	@Override
 	public void updateEvent(String eventID, Event event) {
-		StatefulRedisConnection<String, String> connection = this.redis.connect();
-		RedisStringCommands<String, String> sync = connection.sync();
 		String eventString = gson.toJson(event);
-		sync.set(eventID, eventString);
+		this.redis.set(eventID, eventString);
 	}
 
 	@Override
 	public long deleteEvent(String key) {
-		return 0l; // TODO
+		return this.redis.del(String.valueOf(key));
 	}
 
 	@Override
 	public List<Event> getAllEvents() {
-		StatefulRedisConnection<String, String> connection = this.redis.connect();
-		RedisKeyCommands<String, String> sync = connection.sync();
 		List<Event> eventList = new ArrayList<>();
-		//ToDo: No the optimized way.
-		List<String> keys = sync.keys("*");
+		Set<String> keys = this.redis.keys("*");
 		for (String key: keys) {
 			Event event = getEvent(key);
 			eventList.add(event);
@@ -68,6 +56,6 @@ public class EventServiceImpl implements EventService {
 
 	@Override
 	public String deleteAll() {
-		return null; // TODO
+		return this.redis.flushAll();
 	}
 }
